b3d03f5b01e218b9ea56449ec600325bb2f5d64a
Kevin <kevyohanes@gmail.com>
2026-09-10 11:22:35 +0700

    Refactor Sidebar component: remove Technical Preview tag and update brand header with Document Intelligence

--- ui/src/layout/Sidebar.tsx
+++ ui/src/layout/Sidebar.tsx
@@ -49,14 +49,10 @@ import {
   Layout,
   Menu,
   MenuProps,
-  Tag,
-  Tooltip,
   Typography,
 } from "antd";
 import { useMatchRoute, useNavigate } from "@tanstack/react-router";
 import Images from "src/components/images/Images.ts";
-import LightbulbIcon from "src/cuix/icons/LightbulbIcon";
-import { cdlAmber200, cdlAmber900 } from "src/cuix/variables.ts";
 import "./style.css";
 import AmpUpdateBanner from "src/components/AmpUpdate/AmpUpdateBanner.tsx";
 import { useGetPollingAmpConfig } from "src/api/ampMetadataApi.ts";
@@ -76,12 +72,10 @@ const Sidebar: React.FC = () => {
 
   const navToRagApp = () => {
     navigate({ to: "/chats" }).catch(() => null);
-    return;
   };
 
   const navToData = () => {
     navigate({ to: "/data" }).catch(() => null);
-    return;
   };
 
   const navToAnalytics = () => {
@@ -96,55 +90,6 @@ const Sidebar: React.FC = () => {
   const enableFullUsage = Boolean(!config?.is_valid_config);
 
   const baseItems: MenuItem[] = [
-    {
-      label: collapsed ? (
-        <Tooltip title="Technical Preview">
-          <Tag
-            color={cdlAmber200}
-            style={{
-              borderRadius: 4,
-              height: 24,
-              width: 30,
-              marginLeft: 18,
-            }}
-          >
-            <Flex
-              gap={4}
-              justify="center"
-              align="center"
-              style={{ height: "100%" }}
-            >
-              <LightbulbIcon color="#000" />
-            </Flex>
-          </Tag>
-        </Tooltip>
-      ) : (
-        <Tag
-          color={cdlAmber200}
-          style={{
-            borderRadius: 20,
-            height: 24,
-            paddingLeft: 6,
-            paddingRight: 8,
-            marginLeft: 10,
-          }}
-        >
-          <Flex
-            gap={4}
-            justify="center"
-            align="center"
-            style={{ height: "100%" }}
-          >
-            <LightbulbIcon color="#000" />
-            <Typography.Text style={{ fontSize: 12 }} color={cdlAmber900}>
-              Technical Preview
-            </Typography.Text>
-          </Flex>
-        </Tag>
-      ),
-      key: "tech-preview",
-      type: "group",
-    },
     getItem({
       label: <div data-testid="rag-apps-nav">Chats</div>,
       key: "chat",
@@ -219,21 +164,36 @@ const Sidebar: React.FC = () => {
       width={250}
       ref={ref}
     >
-      <div style={{ padding: 20 }}>
+      {/* Centered Brand Header */}
+      <Flex
+        vertical
+        align="center"
+        justify="center"
+        style={{ padding: "20px 16px", width: "100%" }}
+      >
         <Image
-          src={Images.ClouderaSmall}
+          src={Images.BniIcon}
           preview={false}
-          height={36}
-          style={{ paddingLeft: 4 }}
+          height={32}
+          style={{ objectFit: "contain" }}
         />
-        {!collapsed ? (
-          <Image
-            src={Images.RagStudioProduct}
-            preview={false}
-            style={{ transition: "ease-in", paddingLeft: 5 }}
-          />
-        ) : null}
-      </div>
+        {!collapsed && (
+          <Typography.Text
+            style={{
+              color: "rgba(255, 255, 255, 0.85)",
+              fontWeight: 600,
+              fontSize: 13,
+              lineHeight: 1.2,
+              whiteSpace: "nowrap",
+              marginTop: 6,
+              textAlign: "center",
+            }}
+          >
+            Document Intelligence
+          </Typography.Text>
+        )}
+      </Flex>
+
       <Flex vertical justify="space-between" style={{ height: "85%" }}>
         <Menu selectedKeys={chooseRoute()} mode="inline" items={items} />
         <AmpUpdateBanner isCollapsed={collapsed} />
